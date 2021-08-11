b810a085414321366c39376c6d66bdf403735631
Matheus Leitão <matheus_ramos@outlook.com>
2021-08-11 10:42:49 -0300

    Error

--- kokarDevelopInit.sh
+++ kokarDevelopInit.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo 
+echo "Verificando todas as dependências necessitadas ... ✅"
+git --version > /dev/null 2>&1
+echo $?
+docker-compose --version > /dev/null 2>&1
+echo $?
+python3 --version > /dev/null 2>&1
+echo $?
+node --version > /dev/null 2>&1
+echo $?
+npm --version > /dev/null 2>&1
+echo $?
